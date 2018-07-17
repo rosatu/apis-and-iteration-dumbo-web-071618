3c47a11efddcb740164b2b3b4a181cff22b95d0c
github username <github email address>
2018-07-17 17:32:02 -0400

    this is rosa's and isiah's beautiful star wars code

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,23 +2,38 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
+
 def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
+  character_hash = JSON.parse(all_characters.body)
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
+  individual_character_info = character_hash["results"].find do |character_info_hash|
+    character_info_hash["name"].downcase == character
+  end
+  # collect those film API urls
+  films_array = individual_character_info["films"]
+  # make a web request to each URL to get the info
   #  for that film
+  #first_film = RestClient.get(films_array[0])
+  #films_hash = JSON.parse(first_film.body)
+  films_hash = films_array.map {|url| JSON.parse(RestClient.get(url).body)}
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
 end
-
+  # if don't run through cl, it doesn't downcase name. unless we write downcase, noMethod error
 def parse_character_movies(films_hash)
+#this films_hash works because we define it in the show_character_movies method and call that method in run.rb
+  films_hash.map do |film_info|
+    puts film_info["title"]
+  end
+  # we are putting the return after this end because otherwise it will break the loop
+  return nil
   # some iteration magic and puts out the movies in a nice list
 end
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
   # puts out a welcome message here!
+  puts "WELCOME TO THE STARS!!!"
 end
 
 def get_character_from_user
   puts "please enter a character"
+  character = gets.chomp
   # use gets to capture the user's input. This method should return that input, downcased.
+  return character.downcase 
 end
